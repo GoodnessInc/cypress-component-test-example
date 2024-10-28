1fb1dae2342ec97df04cfac5127c6def7bad0fcf
Robert Reinhard <robert.reinhard@goodness.inc>
2024-10-28 09:24:41 -0700

    Fix typo
    And remove boilerplate

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -1,26 +1,8 @@
-// ***********************************************************
-// This example support/component.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
 // Import commands.js using ES2015 syntax:
 import './commands'
 
-// Add tailwint
+// Add tailwind
 import './component.css'
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
 
 // Augment the Cypress namespace to include type definitions for
 // your custom command.
@@ -33,6 +15,3 @@ declare global {
 	}
 }
 Cypress.Commands.add('mount', mount)
-
-// Example use:
-// cy.mount(<MyComponent />)
